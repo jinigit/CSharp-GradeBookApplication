4edec1859f7cda235ed9dee583f381cec67d50f3
Su Jin Lee <slee@sgi.sk.ca>
2020-04-12 18:33:10 -0600

    Override RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Linq;
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
@@ -10,6 +10,34 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            var boundry = (int)Math.Ceiling(Students.Count * 0.2);
+            var studentAverage = Students.OrderByDescending(x => x.AverageGrade)
+                                .Select(x=>x.AverageGrade).ToArray();
+            try
+            {
+                switch (averageGrade)
+                {
+                    case var d when d >= studentAverage[boundry - 1]:
+                        return 'A';
+                    case var d when d >= studentAverage[2 * boundry - 1]:
+                        return 'B';
+                    case var d when d >= studentAverage[3 * boundry - 1]:
+                        return 'C';
+                    case var d when d >= studentAverage[4 * boundry - 1]:
+                        return 'D';
+                    default:
+                        return 'F';
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
         }
     }
 }
